635bf10ffb9768107b7cda923538ba47c62c0322
eivindholvik <eivind.holvik@noroff.no>
2022-10-28 09:30:18 +0200

    Change joker.js to export a function

--- src/js/joker.js
+++ src/js/joker.js
@@ -1,5 +1,7 @@
 import joker from "give-me-a-joke";
 
-// setInterval(() => {
-  joker.getRandomDadJoke(console.log);
-// }, 10000);
+export function dadJoke() {
+  return new Promise(function (resolve, reject) {
+    joker.getRandomDadJoke(resolve);
+  })
+}
